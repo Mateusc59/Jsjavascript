2082a65c6e0a5aa8f8289e776dac7eb1167b2e74
mathieu hollande <leeroydevassistant8@gmail.com>
2022-06-06 12:12:45 -0400

    OOP JS

---
+++ script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+// 1.New{} is created
+// 2.function is called, this = {}
+// 3.{} linked to prototype
+// 4. function autmatically return {}
+const mathieu = new Person("Mathieu", 1990);
+console.log(mathieu);
+
+console.log(jonas instanceof Person);
+
+//Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+// .prototypeOfLinkedObjects
+
+Person.prototype.species = "Homo Sapiens";
+console.log(jonas.species, mathieu.species);
+console.log(jonas.hasOwnProperty("firstName"));
+console.log(jonas.hasOwnProperty("species"));
+
+console.log(jonas.__proto__);
+//Object.protoype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+const arr = [3, 4, 5, 1, 9, 8, 4, 4, 5, 3];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir((X) => X + 1);
