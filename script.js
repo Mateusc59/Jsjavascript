35ce2c8e92c51fada89913067477d149a107acd2
mathieu hollande <leeroydevassistant8@gmail.com>
2022-06-07 12:51:30 -0400

    Challenge 3 Success

--- script.js
+++ script.js
@@ -1,42 +1,78 @@
 "use strict";
 
+// Coding Challenge #3
+
 /*
-1. Re-create challenge 1, but this time using an ES6 class;
-2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
-3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
-4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
-DATA CAR 1: 'Ford' going at 120 km/h
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
 
-class CarCl {
-  constructor(make, speed) {
-    this.make = make;
-    this.speed = speed;
-  }
-  accelerate() {
-    console.log(`${this.make} is going at ${(this.speed += 10)} mi/h`);
-  }
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
 
-  brake() {
-    console.log(`${this.make} is going at ${(this.speed -= 5)} mi/h`);
-  }
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
 
-  get speedUs() {
-    return this.speed / 1.6;
-  }
+EV.prototype = Object.create(Car.prototype);
+console.log(EV);
 
-  set speedUs(speed) {
-    this.speed = speed * 1.6;
-  }
-}
+EV.prototype = Car.prototype;
+console.log(Car);
 
-const ford = new CarCl("Ford", 120);
-console.log(ford);
-ford.accelerate();
-console.log(ford.speedUs);
+EV.prototype.accelerate = function () {
+  console.log(
+    `This ${
+      this.make
+    } going at ${(this.speed += 20)} km/h with a charge of ${(this.charge -= 1)} %`
+  );
+};
 
-ford.accelerate();
-ford.brake();
-ford.speedUs = 50;
-console.log(ford);
+EV.prototype.brake = function () {
+  console.log(
+    `This ${this.make} going at ${(this.speed -= 5)} km/h with a charge of ${
+      this.charge
+    } %`
+  );
+};
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  console.log(
+    `This renault has ${(this.charge = chargeTo =
+      this.charge)} % of battery left`
+  );
+};
+
+const Renault = new EV("Renault", 100, 90);
+
+const Tesla = new EV("Tesla", 120, 23);
+
+console.log(Car);
+console.log(EV);
+console.log(Tesla);
+Tesla.accelerate();
+Tesla.chargeBattery();
+Tesla.accelerate();
+Tesla.accelerate();
+console.log(Tesla);
+Renault.accelerate();
+Renault.brake();
+Renault.chargeBattery();
+Renault.chargeBattery();
+Renault.brake();
+Renault.brake();
+Renault.brake();
+Renault.accelerate();
+Renault.accelerate();
+Renault.accelerate();
+Tesla.accelerate();
+Tesla.brake();
+Tesla.accelerate();
+Tesla.chargeBattery();
